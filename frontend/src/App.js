ca1a7c946c4cfdac81d72ce30d02fae04e512ef1
Baris Ikinci <58369631+brsikinci@users.noreply.github.com>
2023-11-20 23:25:34 +0300

    Add homepage styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
-import { Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Homepage from "./pages/Homepage";
 import Chat from "./pages/Chat";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Route path="/" Component={Homepage} exact />
-      <Route path="/chats" Component={Chat} exact />
-    </div>
-  );
+    const [userData, setUserData] = useState({
+        username: "",
+        receivername: "",
+        connected: false,
+        message: ""
+    });
+
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" Component={Homepage} exact />
+                <Route path="/chats" Component={Chat} exact />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { TextField, Button, Box, FormControl } from "@mui/material"
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+    let lightGray = "#2c2f33"
+    let darkGray = "#23272a"
+
+    let styles = {
+        loginContainer: {
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            backgroundColor: darkGray
+        },
+
+        loginForm: {
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "30px",
+            padding: "30px",
+            borderRadius: "10px",
+            backgroundColor: lightGray
+        }
+    }
+
+    return (
+        <Box style={styles.loginContainer}>
+            <FormControl style={styles.loginForm}>
+                <TextField id="outlined-basic" label="Username" variant="outlined" borderColor="white" />
+                <Button variant="contained">Connect</Button>
+            </FormControl>
+        </Box>
+    );
 };
 
 export default Homepage;
